c5e3258627ec77c87c6d4871f1542c16a1a01cb9
ky <ra@gmail.com>
2024-10-07 11:21:02 -0300

    listagem

---
+++ listagem/listagem/src/main/java/com/example/listagem/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.listagem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] nomes = new String[]{"Duda","Dudahhh","Dudinha"};
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.ListView);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1, nomes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),Integer.toString(i)+nomes[i],
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
